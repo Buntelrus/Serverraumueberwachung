0dbae03b6f74f65a29a141a78b1fe05f8f9fc52f
buntel <julianbantel@gmail.com>
2024-02-20 23:20:02 +0100

    websocket showcase

---
+++ dashboard/layouts/default.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+console.log('default layout')
+
+</script>
+<template>
+  <slot />
+</template>
--- dashboard/pages/index.vue
+++ dashboard/pages/index.vue
@@ -3,3 +3,7 @@
     <h1>Serverraum Dashboard</h1>
   </div>
 </template>
+
+<script setup lang="ts">
+console.log('index.vue')
+</script>
