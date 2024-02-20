b37d65afab2bbb6f51f4fad09f811392fe7f8958
buntel <julianbantel@gmail.com>
2024-02-20 12:16:40 +0100

    add websocket example

--- surveillance/main.py
+++ surveillance/main.py
@@ -1,6 +1,7 @@
 from typing import Union
 
 from fastapi import FastAPI
+from starlette.websockets import WebSocket
 
 app = FastAPI()
 
@@ -13,3 +14,11 @@ async def root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    while True:
+        data = await websocket.receive_text()
+        await websocket.send_text(f"Message text was: {data}")
