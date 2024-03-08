a0a852e78cf12d6f1c4d774f56b152b67ee3a158
buntel <julianbantel@gmail.com>
2024-03-08 09:48:16 +0100

    fix LED constructor

--- surveillance/gpio/LED.py
+++ surveillance/gpio/LED.py
@@ -3,13 +3,14 @@ from gpiozero import LED as ZeroLED
 from surveillance.gpio.Device import Device
 
 
-class LED(Device, ZeroLED):
+class LED(ZeroLED, Device):
 # class LED(Device):
     def __init__(self, **kwds):
         self.name = 'led'
         self.description = 'A simple LED'
-        self.value = False
-        super().__init__(**kwds)
+        pin = kwds.get('pin')
+        ZeroLED.__init__(pin)
+        Device.__init__(**kwds)
 
     def on(self):
         super().on()
